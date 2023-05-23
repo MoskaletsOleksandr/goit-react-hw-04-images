d723d89a2f9649ffcfe398e73eec827c5062eaef
Олександр <moskaletzsanych@gmail.com>
2023-05-23 20:38:47 +0300

    work-without-button

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,62 @@
-import { useState } from 'react';
 import { Container } from './App.styled';
 import { SearchBar } from 'components/SearchBar';
 import { ImageGallery } from 'components/ImageGallery/ImageGallery';
 import { Modal } from 'components/common/Modal';
 import { Section } from 'components/common/Section';
+import { Button } from 'components/common/Button';
+import { Loader } from 'components/common/Loader';
+import { animateScroll } from 'react-scroll';
+import { fetchPhotos } from '../../services/pixabay-api';
+import { useState, useEffect } from 'react';
 
 export const App = () => {
   const [searchedWord, setSearchedWord] = useState('');
   const [showModal, setShowModal] = useState(false);
   const [largeImageURL, setLargeImageURL] = useState(null);
   const [alt, setAlt] = useState(null);
+  const [images, setImages] = useState([]);
+  const [error, setError] = useState(null);
+  const [totalHits, setTotalHits] = useState(null);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (!searchedWord) {
+      return;
+    }
+
+    setLoading(true);
+    const fetchData = async () => {
+      try {
+        const { hits, totalHits } = await fetchPhotos(searchedWord, page);
+
+        setImages(prevImages => [...prevImages, ...hits]);
+        setTotalHits(totalHits);
+      } catch (error) {
+        setError(error.message);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, [searchedWord, page]);
+
+  const handleMoreBtnClick = async () => {
+    setPage(prevPage => (prevPage += 1));
+    scrollMoreButton();
+  };
+
+  const scrollMoreButton = () => {
+    animateScroll.scrollToBottom({
+      duration: 1000,
+      delay: 10,
+      smooth: 'linear',
+    });
+  };
 
   const handleSearchFormSubmit = searchedWord => {
     setSearchedWord(searchedWord);
+    setPage(1);
+    setImages([]);
   };
 
   const toggleModal = () => {
@@ -29,10 +73,17 @@ export const App = () => {
     <Container>
       <SearchBar onSubmit={handleSearchFormSubmit} />
       <Section>
-        <ImageGallery
-          searchedWord={searchedWord}
-          openModal={openModal}
-        ></ImageGallery>
+        {loading ? (
+          <Loader />
+        ) : (
+          <ImageGallery images={images} openModal={openModal}></ImageGallery>
+        )}
+        {error && <h3>{error}</h3>}
+        {totalHits > images.length && (
+          <Button type="button" loadMore={handleMoreBtnClick}>
+            Load more
+          </Button>
+        )}
       </Section>
       {showModal && (
         <Modal onClose={toggleModal}>
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,104 +1,27 @@
 import PropTypes from 'prop-types';
 import { List } from './ImageGallery.styled';
-import { fetchPhotos } from '../../services/pixabay-api';
-import { useState, useEffect } from 'react';
 import { ImageGalleryItem } from './ImageGalleryItem/ImageGalleryItem';
-import { Button } from 'components/common/Button';
-import { Loader } from 'components/common/Loader';
-import { animateScroll } from 'react-scroll';
 
-const Status = {
-  IDLE: 'idle',
-  PENDING: 'pending',
-  RESOLVED: 'resolved',
-  REJECTED: 'rejected',
-};
-
-export const ImageGallery = ({ openModal, searchedWord }) => {
-  const [images, setImages] = useState([]);
-  const [error, setError] = useState(null);
-  const [status, setStatus] = useState(Status.IDLE);
-  const [totalHits, setTotalHits] = useState(null);
-  const [page, setPage] = useState(1);
-  
-  useEffect(() => {
-    if (!searchedWord) {
-      return;
-    }
-
-    setStatus(Status.PENDING);
-
-    const fetchData = async () => {
-      try {
-        const { hits, totalHits } = await fetchPhotos(searchedWord, page);
-
-    setImages(prevImages => [...prevImages, ...hits]);
-        setTotalHits(totalHits);
-        setStatus(Status.RESOLVED);
-      } catch (error) {
-        setError(error);
-        setStatus(Status.REJECTED);
-      }
-    };
-console.log(page)
-    fetchData();
-  }, [searchedWord, page]);
-
-  const handleMoreBtnClick = async () => {
-    setPage(prevPage => prevPage += 1);
-    scrollMoreButton();
-  };
-
-  const scrollMoreButton = () => {
-    animateScroll.scrollToBottom({
-      duration: 1000,
-      delay: 10,
-      smooth: 'linear',
-    });
-  };
-
-  if (status === Status.IDLE) {
-    return <h3>Enter a search query.</h3>;
-  }
-
-  if (status === Status.PENDING) {
-    return <Loader />;
-  }
-
-  if (status === Status.RESOLVED) {
-    return (
-      <>
-        <List>
-          {images &&
-            images.map(({ id, tags, webformatURL, largeImageURL }) => {
-              return (
-                <ImageGalleryItem
-                  key={id}
-                  alt={tags}
-                  webformatURL={webformatURL}
-                  largeImageURL={largeImageURL}
-                  openModal={openModal}
-                />
-              );
-            })}
-        </List>
-        {totalHits >= images.length && (
-            <Button type="button" loadMore={handleMoreBtnClick}>
-              Load more
-            </Button>
-          )}
-      </>
-    );
-  }
-
-  if (status === Status.REJECTED) {
-    return <h3>{error}</h3>;
-  }
-
-  <h2>header</h2>
-};
+export const ImageGallery = ({ openModal, images }) => {
+  return (
+    <List>
+      {images &&
+        images.map(({ id, tags, webformatURL, largeImageURL }) => {
+          return (
+            <ImageGalleryItem
+              key={id}
+              alt={tags}
+              webformatURL={webformatURL}
+              largeImageURL={largeImageURL}
+              openModal={openModal}
+            />
+          );
+        })}
+    </List>
+  );
+}
 
 ImageGallery.propTypes = {
   openModal: PropTypes.func.isRequired,
-  searchedWord: PropTypes.string.isRequired,
+  images: PropTypes.array.isRequired,
 };
